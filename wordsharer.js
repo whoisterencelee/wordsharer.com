7f65dd4bf97e6b448715f9ad8bbaf4da04dc45ff
Terence Lee <whoisterencelee@hotmail.com>
2014-03-17 17:43:22 +0800

    added github API submodule

---
+++ wordsharer.js
@@ -0,0 +1,2 @@
+var gh=new Github({auth:'oauth',token:'b93365cb0876e6bf85728f3a10e2bab3384d428a'});//give personal access to repo, well repo is public anyways
+var repo=gh.getRepo('whoisterencelee','wordsharer.com');
