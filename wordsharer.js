9e51ce78f3b732002c6960eae2603a85c29156c9
Terence Lee <whoisterencelee@hotmail.com>
2014-03-17 18:52:55 +0800

    can now save md to git

--- wordsharer.js
+++ wordsharer.js
@@ -1,2 +1,6 @@
 var gh=new Github({auth:'oauth',token:'b93365cb0876e6bf85728f3a10e2bab3384d428a'});//give personal access to repo, well repo is public anyways
 var repo=gh.getRepo('whoisterencelee','wordsharer.com');
+
+function save(){
+	repo.write('gh-pages','why.md',C.innerHTML,"commit changes to why.md",function(err){console.log(err)});
+}
