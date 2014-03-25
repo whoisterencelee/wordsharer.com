1c6bbbf9941a1e3b24bdd632a3d1e68ea8ef96e9
Terence Lee <whoisterencelee@hotmail.com>
2014-03-25 16:11:52 +0800

    this version stores output from marked instead of innerhtml

--- wordsharer.js
+++ wordsharer.js
@@ -16,36 +16,27 @@ function wordsharer(words){
 	repo=gh.getRepo('whoisterencelee','wordsharer.com');
 	C=document.getElementById('content');
 	W=words;
-	getWords(W,function(e,text){
-		if(e)C.innerHTML=e;
-		else C.innerHTML=text;
-	});
+	U='.//'+W;
 
 	D=new htmlDiff;
-	D.clearHash();  //needed, initialize html tag hash stores
+	D.clearHash();  // needed, initialize html tag hash stores
+
+	// borrow repo.req to quickly load the content first, not worry about atomic commits
+	repo.req('GET',U,null,function(e,text){
+		if(e){C.innerHTML=errorlog("Unable to load "+W,e);return;}
 	
-}
+		// IS THIS WRONG? because different browser might handle innerHTML differently
+		C.innerHTML=marked(text);// always markup first, follow with innerHTML transform
+		ORIGHTML=C.innerHTML;// always save displayable html
+		// if we use this we need to use a dummy html node to do innerHTML transformation when we're saving ORIGHTML and not displaying
+		*/
 
-function save(){
-	
-	repo.write('gh-pages','why.md',C.innerHTML,"commit changes to why.md",function(err){console.log(err)});
+		ORIGHTML=marked(text);// save reference based on a single output
+		C.innerHTML=ORIGHTML;// for display only
 
-	//OT version
-	//diff the original md with current to get the client insert operation(s)
-		//if content is removed, insert ~~strikeover~~ to convert to insert operation
+		mergeWords();// setup atomic read / write
 
-	//grab the server insert operation from github, using something like git compare
-		//save the current HEAD
-
-	//transform the client insert operation and merge into the latest document
-	//commit
-		//if successful save the current HEAD
-		//otherwise try from grab the 
-}
-
-function getlatest(cb){
-	repo.read('gh-pages','why.md',cb);
-	// function(err, data) {});
+	},'raw');
 
 }
 
@@ -88,37 +79,56 @@ function getWords(file,cb){
 	});
 }
 
-function mergeWords(){
+function mergeWords(cb){
 
 	//2.5 way merge
 
 	//convert any markdown first
-	marked(C.innerHTML,function(e,newhtml){
+	marked(C.innerHTML,function(e,M1){
 		if(e){
-			alert("The edits you entered caused a problem during markdown->html conversion, changes not submitted");
+			alert("The edits you entered caused a problem during markdown->html conversion, changes not submitted : "+e);
 			return;
 		}
 
 		//first merge original into current to see mark what's changed with <ins> <del> and re-instate deleted text
 		//everyone does this so merging other's work later won't need to do this, this is what I mean by 2.5 way
-		var M1=D.diff(ORIGHTML,newhtml);
+		var M2=D.diff(ORIGHTML,M1);
 
 		//getWords and merge in
 		getWords(W,function(change,latest){
 			if(change){alert("ERROR: Cannot merge latest in, changes not submitted "+change);return;}
 
-			if(change===false)M2=M1;//no change, no need to diff
-			else M2=D.diff(latest,M1,{tagless:true});//tagless so we won't get <ins><del></ins>, only merge text, the marks are already done
+			if(change===false)M3=M2;//no change, no need to diff
+			else M3=D.diff(latest,M2,{tagless:true});//tagless so we won't get <ins><del></ins>, only merge text, the marks are already done
 
-			C.innerHTML=M2;//display it to user anyways, because there might be new updates
-			ORIGHTML=M2;//needed this if there's above line, because even if below fails, you might merge again, and you don't want to double mark your changes
+			ORIGHTML=M3;//needed this if there's below line, because even if below fails, you might merge again, and you don't want to double mark your changes
+			C.innerHTML=M3;//display it to user anyways, because there might be new updates
 
-			//write commit
-			save();//TODO to be replace with atomic commit
+			if(typeof cb=='function')cb();
+
 		});
 
 		//insert time modify attribute to all ins and del that doesn't already have this attribute
 	});
+}
+
+function submitWords(){
+
+	mergeWords(function(){
+		repo.write('gh-pages','why.md',marked(C.innerHTML),"commit changes to why.md",function(err){console.log(err)});
+	});
+
+	//OT version
+	//diff the original md with current to get the client insert operation(s)
+		//if content is removed, insert ~~strikeover~~ to convert to insert operation
+
+	//grab the server insert operation from github, using something like git compare
+		//save the current HEAD
+
+	//transform the client insert operation and merge into the latest document
+	//commit
+		//if successful save the current HEAD
+		//otherwise try from grab the 
 }
 
 function errorlog(heading,details){
