6e64693609594e81129f767e0ab2426a9f32f621
Terence Lee <whoisterencelee@hotmail.com>
2014-03-27 22:16:07 +0800

    contenteditable -> contentEditable

--- wordsharer.js
+++ wordsharer.js
@@ -96,12 +96,12 @@ function mergeWords(cb){
 
 	//2.5 way merge, there is only append no delete
 
-	C.contenteditable=false;// lock up, when ever we are going to modify C, otherwise the user might see their modification disappear
+	C.contentEditable=false;// lock up, when ever we are going to modify C, otherwise the user might see their modification disappear
 
 	//convert any markdown first
 	marked(C.innerHTML,function(e,MODIFIED){
 		if(e){
-			C.contenteditable=true;// unlock
+			C.contentEditable=true;// unlock
 			alert("The edits you entered caused a problem during markdown->html conversion, operation aborted : "+e);
 			return;
 		}
@@ -115,7 +115,7 @@ function mergeWords(cb){
 		//getWords and merge in
 		getWords(W,function(change,REMOTE){
 			if(change){
-				C.contenteditable=true;// unlock
+				C.contentEditable=true;// unlock
 				alert("ERROR: Cannot merge REMOTE in. "+change);
 				return;
 			}
@@ -123,7 +123,7 @@ function mergeWords(cb){
 			if(change!=false)STAGED=repairHTML(D.diff(REMOTE,STAGED,{tagless:true}),C);// tagless so we won't get <ins><del></ins>, only merge text, the marks are already done
 			//otherwise STAGED is already the latest
 
-			C.contenteditable=true;// unlock
+			C.contentEditable=true;// unlock
 
 			if(typeof cb=='function')cb(null,STAGED);
 
