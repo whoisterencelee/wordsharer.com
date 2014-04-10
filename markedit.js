215970f3556edcb4882ed067d8eba82a7b77f851
Terence Lee <whoisterencelee@hotmail.com>
2014-04-11 03:26:09 +0800

    markedit heading working

--- markedit.js
+++ markedit.js
@@ -12,45 +12,35 @@
 
 var block = {
 //  newline: /^\n+/,
-  code: /^( {4}[^\n]+\n*)+/,
+  code: noop, ///^( {4}[^\n]+\n*)+/,
   fences: noop,
-  hr: /^( *[-*_]){3,} *(?:\n+|$)/,
-  heading: /^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)/,
+  hr: noop, ///^( *[-*_]){3,} *(?:\n+|$)/,
+  heading: /^ *(#{1,6}) *([^\n|<]+?) *#* *(?=<|\n+|$)/,
+  //heading: noop, ///^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)/,
   nptable: noop,
-  lheading: /^([^\n]+)\n *(=|-){2,} *(?:\n+|$)/,
-  blockquote: /^( *>[^\n]+(\n(?!def)[^\n]+)*\n*)+/,
-  list: /^( *)(bull) [\s\S]+?(?:hr|def|\n{2,}(?! )(?!\1bull )\n*|\s*$)/,
-  html: /^xml/,
+  lheading: noop, ///^([^\n]+)\n *(=|-){2,} *(?:\n+|$)/,
+  blockquote: noop, ///^( *>[^\n]+(\n(?!def)[^\n]+)*\n*)+/,
+  list: noop, ///^( *)(bull) [\s\S]+?(?:hr|def|\n{2,}(?! )(?!\1bull )\n*|\s*$)/,
+  html: /^xml+/,
 //  html: /^ *(?:comment|closed|closing) *(?:\n{2,}|\s*$)/,
-  def: /^ *\[([^\]]+)\]: *<?([^\s>]+)>?(?: +["(]([^\n]+)[")])? *(?:\n+|$)/,
+  def: noop, ///^ *\[([^\]]+)\]: *<?([^\s>]+)>?(?: +["(]([^\n]+)[")])? *(?:\n+|$)/,
   table: noop,
 //  paragraph: /^((?:[^\n]+\n?(?!hr|heading|lheading|blockquote|tag|def))+)\n*/,
   text: /^[^<]+/
 //  text: /^[^\n|<]+/
 };
 
-block.bullet = /(?:[*+-]|\d+\.)/;
-block.item = /^( *)(bull) [^\n]*(?:\n(?!\1bull )[^\n]*)*/;
-block.item = replace(block.item, 'gm')
-  (/bull/g, block.bullet)
-  ();
-
-block.list = replace(block.list)
-  (/bull/g, block.bullet)
-  ('hr', '\\n+(?=\\1?(?:[-*_] *){3,}(?:\\n+|$))')
-  ('def', '\\n+(?=' + block.def.source + ')')
-  ();
-
-block.blockquote = replace(block.blockquote)
-  ('def', block.def)
-  ();
-
 block._tag = '(?!(?:'
   + 'a|em|strong|small|s|cite|q|dfn|abbr|data|time|code'
   + '|var|samp|kbd|sub|sup|i|b|u|mark|ruby|rt|rp|bdi|bdo'
   + '|span|br|wbr|ins|del|img)\\b)\\w+(?!:/|[^\\w\\s@]*@)\\b';
 
-block.xml = '<[^>|\n]*>';
+block.xml = '(<[^>|\n]*>)';
+block.space = '(?:&nbsp;| )';
+
+block.heading = replace(block.heading)
+  (/ /g, block.space)
+  ();
 
 block.html = replace(block.html)
   ('comment', /<!--[\s\S]*?-->/)
@@ -774,7 +764,8 @@ Renderer.prototype.heading = function(text, level, raw) {
     + text
     + '</h'
     + level
-    + '>\n';
+    + '>';
+//    + '>\n';
 };
 
 Renderer.prototype.hr = function() {
